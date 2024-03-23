0696912af09a3c10b19cd2a4d1561747868e82d1
k6mil6 <kamil_6@vk.com>
2024-03-23 16:52:53 +0300

    changing map

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,4 +1,4 @@
-import { useRef, useEffect } from 'react';
+import {useRef, useEffect, useState} from 'react';
 import mapboxgl from 'mapbox-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import './Map.css';
@@ -6,28 +6,35 @@ import './Map.css';
 const MAPBOX_ACCESS_TOKEN = import.meta.env.VITE_MAP_BOX_API_KEY;
 const URL = 'http://localhost:8080/';
 
+// const userID = window.Telegram.WebApp.data.initDataUnsafe.user.id;
+
 const Map = () => {
+    const [user, setUser] = useState([]);
     mapboxgl.accessToken = MAPBOX_ACCESS_TOKEN;
-
-    // const userID = window.Telegram.WebApp.data.initDataUnsafe.user.id;
-
     const userID = 746446622;
 
-    const userLocation =  fetch(
-        URL + '/user?id=' + userID,
-    )
-    console.log(userLocation)
-
     useEffect(() => {
+        const fetchUser = async () => {
+            try {
+                const response = await fetch(`${URL}user?id=${userID}`);
+                const json = await response.json();
+                setUser(json);
+            } catch (error) {
+                console.log(error);
+            }
+        };
+
+        fetchUser();
+
         const map = new mapboxgl.Map({
             container: 'map',
-            style: 'mapbox://styles/mapbox/streets-v11', // Specify the map's style
-            center: [-77.034084, 38.909671], // Initial map center coordinates
+            style: 'mapbox://styles/mapbox/streets-v11',
+            center: [user.longitude, user.latitude],
             zoom: 15,
         });
 
         const marker = new mapboxgl.Marker()
-            .setLngLat([-78, 38.909671]) // Marker coordinates
+            .setLngLat([-78, 38.909671])
             .addTo(map);
 
         marker.getElement().addEventListener('click', () => {
@@ -37,13 +44,14 @@ const Map = () => {
             if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
                 window.open(`maps://?q=${destination.lat},${destination.lng}`);
             } else {
-                window.open(`geo:${destination.lat},${destination.lng}?q=${destination.lat},${destination.lng}`);
+                window.open(
+                    `geo:${destination.lat},${destination.lng}?q=${destination.lat},${destination.lng}`
+                );
             }
         });
-
     }, []);
 
-    return <div id="map" className="map"/>;
+    return <div id="map" className="map" />;
 };
 
 export default Map;
